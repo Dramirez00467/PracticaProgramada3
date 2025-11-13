c5e93cdf3ca5915b7ac65f7652f9f432bebae84d
Camilacr10 <corralesrocacamila@gmail.com>
2025-11-13 14:11:56 -0600

    controller

---
+++ PracticaProgramada3/PracticaProgramada3.DLL/Migrations/20251113201035_InicialCami.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PracticaProgramada3.DLL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InicialCami : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- PracticaProgramada3/PracticaProgramada3/Controllers/VehiculosController.cs
+++ PracticaProgramada3/PracticaProgramada3/Controllers/VehiculosController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PracticaProgramada3.BLL.Servicios;
 
-namespace PracticaProgramada3.Controllers
+namespace ApiVehiculos.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class VehiculosController : Controller
     {
-        public IActionResult Index()
+        private readonly IVehiculosServicio _vehiculosServicio;
+
+        public VehiculosController(IVehiculosServicio vehiculosServicio)
         {
-            return View();
+            _vehiculosServicio = vehiculosServicio;
+        }
+
+        [HttpGet(Name = "ObtenerVehiculos")] // ObtenerTodos
+        public async Task<IActionResult> ObtenerVehiculos()
+        {
+            var respuesta = await _vehiculosServicio.ObtenerVehiculosAsync();
+            return Ok(respuesta);
         }
     }
 }
