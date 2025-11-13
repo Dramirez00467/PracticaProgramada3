da936f97d242872596eaea91d7b0f2866a2ed707
Dramirez00467 <106043587+Dramirez00467@users.noreply.github.com>
2025-11-12 21:24:28 -0600

    Se agregó la migración

--- PracticaProgramada3/PracticaProgramada3.DLL/Repositorio/VehiculosRepositorio.cs
+++ PracticaProgramada3/PracticaProgramada3.DLL/Repositorio/VehiculosRepositorio.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using PracticaProgramada3.DLL.Entidades;
 
 namespace PracticaProgramada3.DLL.Repositorio
