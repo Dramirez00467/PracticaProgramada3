9ee63251d82e6635628e60a5ff2aa658a6296bf4
ValeChacon <valedoki77@hotmail.com>
2025-11-16 16:41:17 -0600

    update 2

--- PracticaProgramada3/PracticaProgramada3/Controllers/VehiculosController.cs
+++ PracticaProgramada3/PracticaProgramada3/Controllers/VehiculosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PracticaProgramada3.BLL.Dtos;
 using PracticaProgramada3.BLL.Servicios;
 
 namespace ApiVehiculos.Controllers
@@ -28,6 +29,30 @@ namespace ApiVehiculos.Controllers
             {
                 return NotFound("Vehículo no encontrado");
             }
+            return Ok(respuesta);
+        }
+        [HttpPost(Name = "CrearVehiculo")] // Crear
+        public async Task<IActionResult> CrearVehiculo(VehiculoDto vehiculo)
+        {
+            var respuesta = await _vehiculosServicio.AgregarVehiculoAsync(vehiculo);
+            if (respuesta.EsError)
+            {
+                return BadRequest(respuesta.Mensaje);
+            }
+            return Ok(respuesta);
+        }
+
+        [HttpPut(Name = "ActualizarVehiculo")] // Actualizar
+        public async Task<IActionResult> ActualizarVehiculo(VehiculoDto vehiculo)
+        {
+            var respuesta = await _vehiculosServicio.ActualizarVehiculoAsync(vehiculo);
+            return Ok(respuesta);
+        }
+
+        [HttpDelete("{id}", Name = "EliminarVehiculo")] // Eliminar
+        public async Task<IActionResult> EliminarVehiculo(int id)
+        {
+            var respuesta = await _vehiculosServicio.EliminarVehiculoAsync(id);
             return Ok(respuesta);
         }
     }
